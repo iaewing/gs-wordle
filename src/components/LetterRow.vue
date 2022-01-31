bde7a0356f3666bbb30fea352548245e24c5dc07
Andrew Noble <a.noble@vehikl.com>
2022-01-31 15:41:06 -0500

    initial commit

---
+++ src/components/LetterRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <LetterTile />
+  <LetterTile />
+  <LetterTile />
+  <LetterTile />
+  <LetterTile />
+</template>
+
+<script >
+import {defineComponent} from "vue";
+import LetterTile from "./LetterTile";
+
+export default defineComponent({
+  name: "LetterRow",
+  components: {
+    LetterTile
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/LetterTile.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="border border-solid border-2 border-gray-500 w-12 h-16">
+h
+</div>
+</template>
+
+<script >
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "LetterTile",
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
