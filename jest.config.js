000275aec27d834f1d752c27f57262eda995e112
Ian <i.ewing@vehikl.com>
2022-02-18 09:45:34 -0500

    WIP - Add test coverage through jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  "collectCoverageFrom": [
+    "**/*.vue",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
+}
