6b3af23ada54c5314fa5bf1cc2c1a5bc578fa930
Ian <i.ewing@vehikl.com>
2022-02-22 09:25:31 -0500

    Increase letter tile test coverage to 100%

--- tests/unit/letter-tile.spec.js
+++ tests/unit/letter-tile.spec.js
@@ -7,4 +7,22 @@ describe('LetterTile.vue', () => {
     const tile = wrapper.findComponent(Tile);
     expect(tile.classes()).toContain('bg-transparent')
   });
+
+  it ('renders green when it receives "correct"', () => {
+    const wrapper = shallowMount(Tile, {propsData: {colour: 'correct'}});
+    const tile = wrapper.findComponent(Tile);
+    expect(tile.classes()).toContain('bg-green-400');
+  });
+
+  it ('renders yellow when it receives "wrong spot"', () => {
+    const wrapper = shallowMount(Tile, {propsData: {colour: 'wrong spot'}});
+    const tile = wrapper.findComponent(Tile);
+    expect(tile.classes()).toContain('bg-yellow-400');
+  });
+
+  it ('renders gray when it receives "not in word"', () => {
+    const wrapper = shallowMount(Tile, {propsData: {colour: 'not in word'}});
+    const tile = wrapper.findComponent(Tile);
+    expect(tile.classes()).toContain('bg-gray-400');
+  });
 });
