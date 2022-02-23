547a9871cbe2fffed5923fd2e1a784b6a10bb238
vehikl <go@vehikl.com>
2022-02-23 16:59:21 -0500

    WIP - Word dictionary verification not quite there

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="bg-black">
     <div class="text-white">
-      <LetterRow :letters="word" :colours="colours"/>
+      <LetterRow :letters="guesses[0].word" :colours="guesses[0].colours"/>
+      <LetterRow :letters="guesses[1].word" :colours="guesses[1].colours"/>
+      <LetterRow :letters="guesses[2].word" :colours="guesses[2].colours"/>
+      <LetterRow :letters="guesses[3].word" :colours="guesses[3].colours"/>
+      <LetterRow :letters="guesses[4].word" :colours="guesses[4].colours"/>
+      <LetterRow :letters="guesses[5].word" :colours="guesses[5].colours"/>
     </div>
     <SimpleKeyboard @onKeyPress="onKeyPress"/>
   </div>
@@ -11,10 +16,7 @@
 <script>
 import LetterRow from "./components/LetterRow";
 import SimpleKeyboard from "@/components/SimpleKeyboard";
-
-//TODO
-// Fix the validateLettersWithColours()
-// it can't deal with five letter words??
+import AllowedWords from 'raw-loader!../wordle-allowed-guesses.txt';
 
 export const FIVE_LETTER_WORDS = [
   'fight',
@@ -24,12 +26,22 @@ export const FIVE_LETTER_WORDS = [
   'lemon',
 ];
 
+
 export default {
   name: 'App',
   data() {
     return {
-      word: [],
-      colours: []
+      guesses: [
+        {word: [], colours: []},
+        {word: [], colours: []},
+        {word: [], colours: []},
+        {word: [], colours: []},
+        {word: [], colours: []},
+        {word: [], colours: []},
+      ],
+      currentGuess: 0,
+      isCorrect: false,
+      wordList: "",
     }
   },
   components: {
@@ -39,38 +51,44 @@ export default {
   methods: {
     validateLettersWithColours() {
       const rightAnswer = ("water").toUpperCase().split('');
-      // console.log(this.colours)
-      // this.colours.push('before map')
-      // console.log(this.colours)
-      this.word.map(function (letter, index) {
+
+      this.guesses[this.currentGuess].word.map(function (letter, index) {
         console.log(letter, index, rightAnswer[index]);
         if (letter === rightAnswer[index]) {
           console.log('checking right answer');
-          this.colours.push('correct');
+          this.guesses[this.currentGuess].colours.push('correct');
         } else if (rightAnswer.includes(letter)) {
           console.log('checking wrong answer');
-          this.colours.push('wrong spot');
+          this.guesses[this.currentGuess].colours.push('wrong spot');
         } else {
           console.log('checking for no answer');
-          this.colours.push('not in word');
+          this.guesses[this.currentGuess].colours.push('not in word');
         }
       }, this)
     },
-
     onKeyPress(button) {
       if (button === "{enter}") {
         this.submit();
       } else if (button === "{bksp}") {
-        this.word.pop();
+        this.guesses[this.currentGuess].word.pop();
       } else {
-        this.word.push(button.toUpperCase());
+        this.guesses[this.currentGuess].word.push(button.toUpperCase());
       }
     },
     submit() {
-      if (this.word.length !== 5) {
+      if (this.guesses[this.currentGuess].word.length !== 5 || this.checkWord() !== true) {
         return;
       }
       this.validateLettersWithColours();
+
+      this.currentGuess += 1;
+      if (this.guesses[this.currentGuess].colours.filter(color => color != 'correct').length === 0) {
+        alert('You won in ' + this.currentGuess + ' tries');
+      }
+    },
+    checkWord()
+    {
+      return AllowedWords.match(this.guesses[this.currentGuess].word);
     }
   }
 }
